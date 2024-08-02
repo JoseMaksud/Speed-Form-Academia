6d195615b7176c88f6441d59632aee97a6306f48
JoseMaksud <jf9718830@gmail.com>
2024-08-02 11:40:46 -0300

    Scroll subindo

--- Speed Form Academias/script.js
+++ Speed Form Academias/script.js
@@ -11,3 +11,33 @@ cardsContainer.addEventListener("click", (e) => {
 
   target.classList.add("active");
 });
+
+let lastScrollTop = 0;
+let timer;
+const header = document.querySelector('header');
+
+function hideHeader() {
+    header.style.top = "-85px";
+}
+
+function showHeader() {
+    header.style.top = "0";
+}
+
+const inactivityTime = 1200;
+
+window.addEventListener('scroll', function () {
+    clearTimeout(timer);
+
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (currentScroll > lastScrollTop) {
+        hideHeader();
+    } else {
+        showHeader();
+    }
+
+    timer = setTimeout(showHeader, inactivityTime);
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+});
