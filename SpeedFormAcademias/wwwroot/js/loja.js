0ae14f56a4f73782f68bd9ecde7ac8ee150147a6
JoseMaksud <jf9718830@gmail.com>
2024-11-27 11:38:36 -0300

    Atualizando site e responsividade.

--- SpeedFormAcademias/wwwroot/js/loja.js
+++ SpeedFormAcademias/wwwroot/js/loja.js
@@ -1,13 +1,36 @@
-const cardsContainer = document.querySelector(".containerCarrousel");
-
-cardsContainer.addEventListener("click", (e) => {
-  const target = e.target.closest(".card");
-
-  if (!target) return;
-
-  cardsContainer.querySelectorAll(".card").forEach((card) => {
-    card.classList.remove("active");
-  });
-
-  target.classList.add("active");
+var swiper = new Swiper(".slide-container", {
+  slidesPerView: 4,
+  spaceBetween: 20,
+  sliderPerGroup: 4,
+  loop: true,
+  centerSlide: "true",
+  fade: "true",
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1, 
+      slidesPerGroup: 1, 
+    },
+    520: {
+      slidesPerView: 2,
+      slidesPerGroup: 1, 
+    },
+    768: {
+      slidesPerView: 3,
+      slidesPerGroup: 1, 
+    },
+    1000: {
+      slidesPerView: 4,
+      slidesPerGroup: 4, 
+    },
+  },
 });
